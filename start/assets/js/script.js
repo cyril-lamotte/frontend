83a7e547431837c43f2ac758d4025264eff264ee
Cyril Lamotte <lamotte.cyril@gmail.com>
2013-05-31 17:09:11 +0200

    Gestion des polyfills

--- start/assets/js/script.js
+++ start/assets/js/script.js
@@ -4,10 +4,36 @@
 $(document).ready(function() {
 
 
+	// Gérer les Polyfills
+	managePolyfills()
+
+
+
 
 }); // /ready
 
 
+
+
+
+
+
+// Chargement des scripts fallback uniquement en cas de besoin
+var managePolyfills = function () {
+
+	Modernizr.load({
+	  test: Modernizr.input.placeholder,
+	  nope: 'assets/js/plugins/jquery.placeholder.min.js',
+	  complete : function() {
+
+	    // Placeholder
+	    if( $.fn.placeholder )
+	      $('input[placeholder]').placeholder();
+
+	  }
+	});
+
+}
 
 
 
