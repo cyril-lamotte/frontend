94960979c449f22e2bc5c1f02cc39101310ea26c
Cyril Lamotte <lamotte.cyril@gmail.com>
2013-06-06 15:46:40 +0200

    Ajout du plugin pop

---
+++ jQuery-plugins/jquery.pop.js
@@ -0,0 +1,147 @@
+
+(function( $ ){
+
+
+	$.pop = function(element, options) {
+
+		// Options par défaut
+		var defaults = {
+			delay: 0,
+			positionMy: 'center top',
+			positionAt: 'center bottom+20',
+			onShow: function() {},
+			onHide: function() {}
+		};
+
+		var plugin = this;
+
+		plugin.settings = {}
+
+		var $element = $(element),
+				element = element;
+
+
+		plugin.init = function() {
+
+			// Ecrasement avec les options utilisateur
+			plugin.settings = $.extend({}, defaults, options);
+
+			plugin.settings.$trigger = $element;
+			plugin.settings.$target  = $('#'+ $element.data('pop-target'));
+
+
+			// Masquer le pop à l'initialisation
+			hide();
+			setFlag(false);
+
+			// Définir les évènements
+			setEvents();
+
+		}
+
+
+		// Afficher le pop
+		var show = function() {
+			plugin.settings.$target.show();
+			plugin.settings.onShow();
+		}
+
+
+		// Masquer le pop
+		var hide = function() {
+			plugin.settings.$target.hide();
+			plugin.settings.onHide();
+		}
+
+		// Masquer le pop avec temporisation
+		var hideWithTimeout = function() {
+
+			var id = plugin.settings.$target.attr('id');
+			var timeoutID = window.setTimeout('$.fn.popHide("'+ id +'")', plugin.settings.delay);
+
+		}
+
+		// Positionner le pop
+		var position = function() {
+
+			plugin.settings.$target
+				.position({
+					of: plugin.settings.$trigger,
+					my: plugin.settings.positionMy,
+					at: plugin.settings.positionAt
+				});
+
+		}
+
+
+		// Définir le flag
+		var setFlag = function(flag) {
+			plugin.settings.$target.data('flag', flag);
+		}
+
+		var setEvents = function() {
+
+
+			// Placer un flag au survol du pop
+			plugin.settings.$target.hover(function() {
+				setFlag(true);
+			}, function() {
+				setFlag(false);
+
+				hideWithTimeout();
+
+			});
+
+
+			// Afficher / Masquer au survol du trigger
+			plugin.settings.$trigger.hover(function() {
+
+				show();
+				position();
+
+			}, function() {
+
+				// Masquer si la souris ne survole pas
+				if( ! plugin.settings.$target.data('flag') )
+				{
+					hideWithTimeout();
+				}
+
+			});
+
+
+		}
+
+
+		plugin.init();
+
+	}
+
+
+
+  $.fn.pop = function(options) {
+
+		return this.each(function() {
+
+			if (undefined == $(this).data('pop')) {
+
+				var plugin = new $.pop(this, options);
+				$(this).data('pop', plugin);
+
+			}
+
+		});
+
+ 	};
+
+
+ 	$.fn.popHide = function(id) {
+
+ 		if( ! $('#'+ id).data('flag') )
+ 			$('#'+ id).hide();
+ 	}
+
+
+
+
+})( jQuery );
