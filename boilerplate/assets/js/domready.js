66f0766bd1ff1b5a8f0a3619e487ba7f0f798e1a
Cyril Lamotte <lamotte.cyril@gmail.com>
2015-05-13 15:48:40 +0200

    Add JS NameSpace

--- boilerplate/assets/js/domready.js
+++ boilerplate/assets/js/domready.js
@@ -3,37 +3,10 @@
 /* Execute code when the DOM is fully loaded */
 $(document).ready(function() {
 
+  app.ui.skipLinks();
 
-  //warningCNIL();
+  app.polyfills();
 
-  //managePopins();
-
-  //managePolyfills();
-
-  // Text resizing
-  //textTools('main');
-
-  // Equalize columns
-  //$('div.article-item').equalize();
-
-/*
-  // Sliding Panel
-  slidingPanel({
-    overlay: false,
-    wrapperSlide: false,
-    position: 'left',
-  });
-*/
-
-  // Collapsible menu
-/*
-  // Close all
-  $('div.nav-menu .expanded').toggleClass('expanded collapsed').find('> ul').hide();
-
-  collapsibleList({
-    list: $('div.nav-menu ul')
-  });
-*/
 
 }); // /ready
 
