eb32b24b4b004ab0227b0a2e01880b22a05c1a1b
LAMOTTE Cyril <clamotte@jouve.fr>
2016-04-13 09:29:02 +0200

    Equalize 1.3.0 > Range 2px

--- boilerplate/plugins/dev/jquery.equalize/jquery.equalize.js
+++ boilerplate/plugins/dev/jquery.equalize/jquery.equalize.js
@@ -1,7 +1,7 @@
 (function( $ ){
 
   /**
-  * Plugin jQuery equalize v1.2.0
+  * Plugin jQuery equalize v1.3.0
   * @link https://github.com/cyril-lamotte/frontend/tree/master/boilerplate/plugins/dev/jquery.equalize
   */
 
@@ -12,24 +12,44 @@
 
     $(this).each(function(i, el) {
 
-      var offsetTop = $(el).offset().top;
-      var offsetLabel = 'offset-'+ Math.ceil(offsetTop);
+      var offsetTop = Math.round( $(el).offset().top );
+      var offsetLabel = 'offset-'+ offsetTop;
+      var offsetLabelRounded = null;
+
 
       // Create table
-      if( ! arrayLines[offsetLabel] ) {
+      // 2px range
+      if( ! arrayLines[offsetLabel] && ! arrayLines['offset-'+ (offsetTop + 1)] && ! arrayLines['offset-'+ (offsetTop - 1)] ) {
         arrayLines[offsetLabel] = Array();
         arrayLines[offsetLabel].push(el);
+
+        // This offset doesn't exists, add a "new line" class
+        $(el).addClass('equalize--new-line');
       }
       else {
-        arrayLines[offsetLabel].push(el);
-        var length = arrayLines[offsetLabel].length;
 
-        minHeight = Math.max(arrayLines[offsetLabel][0].offsetHeight, this.offsetHeight);
+        if( arrayLines[offsetLabel] ) {
+          offsetLabelRounded = offsetLabel;
+        }
+        else if( arrayLines['offset-'+ (offsetTop + 1)] ) {
+          offsetLabelRounded = 'offset-'+ (offsetTop + 1);
+        }
+        else if( arrayLines['offset-'+ (offsetTop - 1)] ) {
+          offsetLabelRounded = 'offset-'+ (offsetTop - 1);
+        }
+
+
+
+        arrayLines[offsetLabelRounded].push(el);
+        var length = arrayLines[offsetLabelRounded].length;
+
+        minHeight = Math.max(arrayLines[offsetLabelRounded][0].offsetHeight, this.offsetHeight);
 
         // Apply minHeight
-        $.each(arrayLines[offsetLabel], function(i, el) {
-          $(el).css('minHeight', minHeight).addClass('equalized');
+        $.each(arrayLines[offsetLabelRounded], function(i, el) {
+          $(el).css('minHeight', minHeight+1).addClass('equalized');
         });
+
       }
 
 
