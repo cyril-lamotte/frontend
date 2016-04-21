f213714e716e5bf7b250079f3dd6161283161eb2
LAMOTTE Cyril <clamotte@jouve.fr>
2016-04-21 16:46:16 +0200

    Update base-theme

--- boilerplate/tools/Drupal/base-theme/templates/components/footer.tpl.php
+++
@@ -1,6 +0,0 @@
-<footer class="main-footer" role="contentinfo">
-    <div id="footer"><div class="section">
-      <?php print render($page['footer']); ?>
-    </div>
-  </div> <!-- /.section, /#footer -->
-</footer>
